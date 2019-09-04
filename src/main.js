1fea1d9a93efa422710268918fe18c03b17f0240
Лончук Вячеслав Петрович <v.lonchuk@hq.qiwi.com>
2019-09-05 00:21:24 +0300

    module5-task2

--- src/main.js
+++ src/main.js
@@ -3,15 +3,11 @@ import Menu from './components/menu';
 import TripInfo from './components/trip-info';
 import TripCost from './components/trip-cost';
 import Filter from './components/filter';
-import Sort from './components/sort';
-import EditEvent from './components/edit-event';
-import Event from './components/event';
-import NoEvent from './components/no-event';
+import TripController from './components/trip-controller';
 
 import {generateTripRoute} from './data/trip-route';
 import menuData from './data/menu';
 import filterData from './data/filter';
-import columns from './data/columns';
 
 const tripMain = document.querySelector(`.trip-main`);
 const tripInfoContainer = tripMain.querySelector(`.trip-main__trip-info `);
@@ -24,49 +20,12 @@ const tripInfo = new TripInfo(tripRoute);
 const tripCost = new TripCost(tripRoute);
 const menu = new Menu(menuData);
 const filter = new Filter(filterData);
-const sort = new Sort(columns);
-const noEvent = new NoEvent();
 
 utils.render(tripInfoContainer, tripInfo.getElement(), utils.Position.BEFOREEND);
 utils.render(tripInfoContainer, tripCost.getElement(), utils.Position.BEFOREEND);
 utils.render(menuContainer, menu.getElement(), utils.Position.BEFOREEND);
 utils.render(menuContainer, filter.getElement(), utils.Position.BEFOREEND);
-if (tripRoute.length > 0) {
-  utils.render(tripEvents, sort.getElement(), utils.Position.BEFOREEND);
-} else {
-  utils.render(tripEvents, noEvent.getElement(), utils.Position.BEFOREEND);
-}
 
-for (let event of tripRoute) {
-  let eventComponent = new Event(event);
-  let editEventComponent = new EditEvent(event);
+const tripController = new TripController(tripEvents, tripRoute);
+tripController.init();
 
-  let btn = eventComponent.getElement().querySelector(`.event__rollup-btn`);
-  let closeEditEventHandler = function () {
-    editEventComponent.getElement().parentElement.replaceChild(eventComponent.getElement(), editEventComponent.getElement());
-    document.removeEventListener(`keydown`, keydownEventHandler);
-  };
-  let keydownEventHandler = function (evt) {
-    if (evt.keyCode === utils.Keys.ESCAPE) {
-      closeEditEventHandler();
-    }
-  };
-  let openEditEventHandler = function () {
-    const e = new KeyboardEvent(`keydown`, {
-      bubbles: true,
-      keyCode: utils.Keys.ESCAPE
-    });
-    document.dispatchEvent(e);
-
-    eventComponent.getElement().parentElement.replaceChild(editEventComponent.getElement(), eventComponent.getElement());
-    document.addEventListener(`keydown`, keydownEventHandler);
-  };
-  btn.addEventListener(`click`, openEditEventHandler);
-
-  btn = editEventComponent.getElement().querySelector(`.event__rollup-btn`);
-  btn.addEventListener(`click`, closeEditEventHandler);
-  btn = editEventComponent.getElement().querySelector(`.event__save-btn`);
-  btn.addEventListener(`click`, closeEditEventHandler);
-
-  utils.render(tripEvents, eventComponent.getElement(), utils.Position.BEFOREEND);
-}
